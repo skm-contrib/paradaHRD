dba90e3646d52abb286a350aefe206a016b1522f
Maks <petrunmaks@gmail.com>
2023-04-15 10:42:24 +0300

    config changed new again 2

--- resources/js/components/mainPage.vue
+++ resources/js/components/mainPage.vue
@@ -10,7 +10,7 @@
       </div>
 
       <div>
-        <h1 class="text-5xl font-sofia">Відділ кадрів</h1>
+        <h1 class="text-4xl font-sofia">Відділ кадрів</h1>
         <p
           class="text-2xl font-bold text-center text-neutral-700 visible sm:invisible"
           @click="openInfo()"
