f685c731100ef1bd637e73cfe4efd186601b2d68
Maks <petrunmaks@gmail.com>
2023-04-18 21:13:54 +0300

    changed js minification

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,16 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
+
 });
