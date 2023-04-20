52934a35bee7ade86c04cf16feb33c6a1949cc33
Maks <petrunmaks@gmail.com>
2023-04-20 13:33:41 +0300

    new build config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import 'vite/modulepreload-polyfill'
 
 export default defineConfig({
     plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
         }),
     ],
     build: {
+        manifest: true,
         outDir: './dist'
     },
 
